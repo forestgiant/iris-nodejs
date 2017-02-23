d81ebf11cac7d8dac602fee2f469d8dd1c01e02a
Stephan Baker <stephan.baker@gmail.com>
2017-02-23 18:18:19 -0500

    Cleaning up nesting problem in example.js

--- example.js
+++ example.js
@@ -3,46 +3,31 @@ const textEncoding = require('text-encoding');
 
 function main() {
     var client = new iris_client("127.0.0.1:32000", "")
-    client.connect().then(function(response){
-        console.log("Session: ", response.session)
-
-        var encodedDev = new textEncoding.TextEncoder("utf-8").encode("stephan");
-        client.setValue("company", "dev", encodedDev).then(function(response){
-            var decodedSetDevResponse = new textEncoding.TextDecoder('utf-8').decode(response.value)
+    client.connect()
+        .then(function(connectResponse){
+            console.log("Session: ", connectResponse.session)
+            var encodedDev = new textEncoding.TextEncoder("utf-8").encode("stephan");
+            return client.setValue("company", "dev", encodedDev)
+        }).then(function(setResponse){
+            var decodedSetDevResponse = new textEncoding.TextDecoder('utf-8').decode(setResponse.value)
             console.log("SetValueResponse: ", decodedSetDevResponse) 
-
             var encodedDesigner = new textEncoding.TextEncoder("utf-8").encode("rich");
-            client.setValue("company", "designer", encodedDesigner).then(function(response){
-                var decodedSetDesignerResponse = new textEncoding.TextDecoder('utf-8').decode(response.value)
-                console.log("SetValueResponse: ", decodedSetDesignerResponse) 
-
-                client.getValue("company", "designer").then(function(response){
-                    var decodedGetDesignerResponse = new textEncoding.TextDecoder('utf-8').decode(response.value)
-                    console.log("GetValueResponse: ", decodedGetDesignerResponse)
-
-                    client.removeValue("company", "designer").then(function(response) {
-                        console.log("RemoveValueResponse: ", response)
-
-                        client.removeSource("company").then(function(response) {
-                            console.log("RemoveSourceResponse: ", response)  
-                        }, function(error) {
-                            console.log(error)
-                        })
-                    }, function(error) {
-                        console.log(error)
-                    })
-                }, function(error){
-                    console.log(error)
-                }) 
-            }, function(error){
-                console.log(error)
-            })
-        }, function(error) {
+            return client.setValue("company", "designer", encodedDesigner)
+        }).then(function(setResponse){
+            var decodedSetDesignerResponse = new textEncoding.TextDecoder('utf-8').decode(setResponse.value)
+            console.log("SetValueResponse: ", decodedSetDesignerResponse)
+            return client.getValue("company", "designer")
+        }).then(function(getResponse){
+            var decodedGetDesignerResponse = new textEncoding.TextDecoder('utf-8').decode(getResponse.value)
+            console.log("GetValueResponse: ", decodedGetDesignerResponse)
+            return client.removeValue("company", "designer")
+        }).then(function(removeValueResponse){
+            console.log("RemoveValueResponse: ", removeValueResponse)
+            return client.removeSource("company")
+        }).then(function(removeSourceResponse){
+            console.log("RemoveSourceResponse: ", removeSourceResponse) 
+        }).catch(function(error){
             console.log(error)
         })
-    }, function(error) {
-        console.log(error)
-    })
 }
-
-main();
+main()
