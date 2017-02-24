0cecb7ef2f2e1a73c2c41f274972ffe92b8fbe26
Stephan Baker <stephan.baker@gmail.com>
2017-02-24 14:49:44 -0500

    Getting consistent with semicolon usage.

--- api.js
+++ api.js
@@ -1,13 +1,13 @@
 const messages = require('./iris_pb.js');
-const services = require('./iris_grpc_pb.js')
-const grpc = require('grpc')
+const services = require('./iris_grpc_pb.js');
+const grpc = require('grpc');
 
-const defaultIrisPort = 32000
+const defaultIrisPort = 32000;
 
 class IrisClient {
     constructor(serverAddress, caFile) {
-        this.serverAddress = serverAddress
-        var caFile
+        this.serverAddress = serverAddress;
+        var caFile;
         if (!caFile || caFile.length == 0) {
             this.creds = grpc.credentials.createInsecure();
         } else {
@@ -18,21 +18,21 @@ class IrisClient {
     connect() {
         return new Promise((resolve, reject) => {
             if(this.serverAddress.length == 0) {
-                reject(Error("You must provide a server address to connect to."))
+                reject(Error("You must provide a server address to connect to."));
                 return
             }
 
             this.rpc = new services.IrisClient(this.serverAddress, this.creds);
-            const connectReq = new messages.ConnectRequest()
+            const connectReq = new messages.ConnectRequest();
             this.rpc.connect(connectReq, function(err, response) {
                 if (err) {
-                    reject(Error("Failed to connect to Iris server at " + this.serverAddress))
+                    reject(Error("Failed to connect to Iris server at " + this.serverAddress));
                     return
                 } else {
-                    this.session = response.getSession()
+                    this.session = response.getSession();
                     resolve({
                         "session":response.getSession()
-                    })
+                    });
                     return
                 }  
             })
@@ -41,114 +41,114 @@ class IrisClient {
 
     setValue(source, key, value) {
         return new Promise((resolve, reject) => {
-            const setReq = new messages.SetValueRequest()
-            setReq.setSession(this.session)
-            setReq.setSource(source)
-            setReq.setKey(key)
-            setReq.setValue(value)
+            const setReq = new messages.SetValueRequest();
+            setReq.setSession(this.session);
+            setReq.setSource(source);
+            setReq.setKey(key);
+            setReq.setValue(value);
             this.rpc.setValue(setReq, function(err, response) {
                 if (err) {
-                    reject(err)
+                    reject(err);
                 } else {
                     resolve({
                         "value":response.getValue()
-                    })
+                    });
                 }
-            })
+            });
         })
     }
 
     getValue(source, key) {
         return new Promise((resolve, reject) => {
-            const getReq = new messages.GetValueRequest()
-            getReq.setSession(this.session)
-            getReq.setSource(source)
-            getReq.setKey(key)
+            const getReq = new messages.GetValueRequest();
+            getReq.setSession(this.session);
+            getReq.setSource(source);
+            getReq.setKey(key);
             this.rpc.getValue(getReq, function(err, response) {
                 if (err) {
-                    reject(err)
+                    reject(err);
                 } else {
                     resolve({
                         "value":response.getValue()
-                    })
+                    });
                 }
-            })
+            });
         })
     }
 
     removeValue(source, key) {
         return new Promise((resolve, reject) => {
-            const removeReq = new messages.RemoveValueRequest()
-            removeReq.setSession(this.session)
-            removeReq.setSource(source)
-            removeReq.setKey(key)
+            const removeReq = new messages.RemoveValueRequest();
+            removeReq.setSession(this.session);
+            removeReq.setSource(source);
+            removeReq.setKey(key);
             this.rpc.removeValue(removeReq, function(err, response) {
                 if (err) {
-                    reject(err)
+                    reject(err);
                 } else {
                     resolve({
                         "session":response.getSession(),
                         "source":response.getSource(),
                         "key":response.getKey()
-                    })
+                    });
                 }
-            })
+            });
         })
     }
 
     removeSource(source) {
         return new Promise((resolve, reject) => {
-            const removeReq = new messages.RemoveSourceRequest()
-            removeReq.setSession(this.session)
-            removeReq.setSource(source)
+            const removeReq = new messages.RemoveSourceRequest();
+            removeReq.setSession(this.session);
+            removeReq.setSource(source);
             this.rpc.removeSource(removeReq, function(err, response) {
                 if (err) {
-                    reject(err)
+                    reject(err);
                 } else {
                     resolve({
                         "session":response.getSession(),
                         "source":response.getSource()
-                    })
+                    });
                 }
-            })
+            });
         })
     }
 
     getSources(){
         return new Promise((resolve, reject) => {
-            const request = new messages.GetSourcesRequest()
-            request.setSession(this.session)
+            const request = new messages.GetSourcesRequest();
+            request.setSession(this.session);
 
-            var sources = []
+            var sources = [];
             var call = this.rpc.getSources(request);
             call.on('data', response => {
-                sources.push(response.getSource())
+                sources.push(response.getSource());
             });
             call.on('end', () => {
-                resolve(sources)
+                resolve(sources);
             });
             call.on('error', err => {
-                reject(err)
+                reject(err);
             });
         })
     }
 
     getKeys(source) {
         return new Promise((resolve, reject) => {
-            const request = new messages.GetKeysRequest()
-            request.setSession(this.session)
-            request.setSource(source)
+            const request = new messages.GetKeysRequest();
+            request.setSession(this.session);
+            request.setSource(source);
 
-            var keys = []
-            var call = this.rpc.getKeys(request)
+            var keys = [];
+            var call = this.rpc.getKeys(request);
             call.on('data', response => {
-                keys.push(response.getKey())
+                keys.push(response.getKey());
             });
             call.on('end', () => {
-                resolve(keys)
+                resolve(keys);
             });
             call.on('error', err => {
-                reject(err)
+                reject(err);
             });
         })
     }
--- example.js
+++ example.js
@@ -1,39 +1,39 @@
-const iris_client = require("./api.js")
+const iris_client = require("./api.js");
 const textEncoding = require('text-encoding');
 
 function main() {
-    var client = new iris_client("127.0.0.1:32000", "")
+    var client = new iris_client("127.0.0.1:32000", "");
     client.connect()
     .then(function(connectResponse){
-        console.log("Session: ", connectResponse.session)
+        console.log("Session: ", connectResponse.session);
         var encodedDev = new textEncoding.TextEncoder("utf-8").encode("stephan");
         return client.setValue("company", "dev", encodedDev)
     }).then(function(setResponse){
-        var decodedSetDevResponse = new textEncoding.TextDecoder('utf-8').decode(setResponse.value)
-        console.log("SetValueResponse: ", decodedSetDevResponse) 
+        var decodedSetDevResponse = new textEncoding.TextDecoder('utf-8').decode(setResponse.value);
+        console.log("SetValueResponse: ", decodedSetDevResponse) ;
         var encodedDesigner = new textEncoding.TextEncoder("utf-8").encode("rich");
         return client.setValue("company", "designer", encodedDesigner)
     }).then(function(setResponse){
-        var decodedSetDesignerResponse = new textEncoding.TextDecoder('utf-8').decode(setResponse.value)
-        console.log("SetValueResponse: ", decodedSetDesignerResponse)
+        var decodedSetDesignerResponse = new textEncoding.TextDecoder('utf-8').decode(setResponse.value);
+        console.log("SetValueResponse: ", decodedSetDesignerResponse);
         return client.getValue("company", "designer")
     }).then(function(getResponse){
-        var decodedGetDesignerResponse = new textEncoding.TextDecoder('utf-8').decode(getResponse.value)
-        console.log("GetValueResponse: ", decodedGetDesignerResponse)
+        var decodedGetDesignerResponse = new textEncoding.TextDecoder('utf-8').decode(getResponse.value);
+        console.log("GetValueResponse: ", decodedGetDesignerResponse);
         return client.getSources()
     }).then(function(sources){
-        console.log("Sources: ", sources)
+        console.log("Sources: ", sources);
         return client.getKeys("company")
     }).then(function(keys){
-        console.log("Keys: ", keys)
+        console.log("Keys: ", keys);
         return client.removeValue("company", "designer")
     }).then(function(removeValueResponse){
-        console.log("RemoveValueResponse: ", removeValueResponse)
+        console.log("RemoveValueResponse: ", removeValueResponse);
         return client.removeSource("company")
     }).then(function(removeSourceResponse){
-        console.log("RemoveSourceResponse: ", removeSourceResponse) 
+        console.log("RemoveSourceResponse: ", removeSourceResponse);
     }).catch(function(error){
-        console.log(error)
+        console.log(error);
     })
 }
 main()
